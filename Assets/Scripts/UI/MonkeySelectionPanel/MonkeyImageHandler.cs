58ea83b5741861d0acca5ff26960cc37f26978a0
Shakil Hassan <50647411+Shakil-Hassan@users.noreply.github.com>
2024-04-20 23:53:56 +0530

    Monkey not moving with cursor issue fixed

--- Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
+++ Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
@@ -6,11 +6,14 @@ using UnityEngine.UI;
 
 namespace ServiceLocator.UI
 {
-    public class MonkeyImageHandler : MonoBehaviour
+    public class MonkeyImageHandler : MonoBehaviour, IDragHandler, IEndDragHandler, IPointerDownHandler
     {
         private Image monkeyImage;
         private MonkeyCellController owner;
         private Sprite spriteToSet;
+        private RectTransform rectTransform;
+        private Vector3 originalPosition;
+        private Vector3 originalAnchorPosition;
 
         public void ConfigureImageHandler(Sprite spriteToSet, MonkeyCellController owner)
         {
@@ -22,6 +25,40 @@ namespace ServiceLocator.UI
         {
             monkeyImage = GetComponent<Image>();
             monkeyImage.sprite = spriteToSet;
+            rectTransform = GetComponent<RectTransform>();
+            originalPosition = rectTransform.position;
+            originalAnchorPosition = rectTransform.anchoredPosition;
+
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            Vector2 localPoint;
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(transform.parent as RectTransform, eventData.position, eventData.pressEventCamera, out localPoint))
+            {
+                rectTransform.localPosition = localPoint + eventData.delta / transform.parent.localScale;
+                owner.MonkeyDraggedAt(rectTransform.position);
+            }
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            ResetMonkey();
+            owner.MonkeyDroppedAt(eventData.position);
+        }
+
+        private void ResetMonkey()
+        {
+            monkeyImage.color = new Color(1, 1, 1, 1);
+            rectTransform.position = originalPosition;
+            rectTransform.anchoredPosition = originalAnchorPosition;
+            GetComponent<LayoutElement>().enabled = false;
+            GetComponent<LayoutElement>().enabled = true;
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            monkeyImage.color = new Color(1,1,1,0.6f);
         }
     }
 }
