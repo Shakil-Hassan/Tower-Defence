a8431e7b8cd89ed3260d35da334c896b8db16d13
Shakil Hassan <50647411+Shakil-Hassan@users.noreply.github.com>
2024-05-01 21:00:32 +0530

    Implemented Service Locator and centrelized Player Service

---
+++ Assets/Scripts/Main/GameService.cs
@@ -0,0 +1,26 @@
+﻿using ServiceLocator.Player;
+using ServiceLocator.Utilities;
+using UnityEngine;
+
+namespace Assets.Scripts.Main
+{
+    public class GameService : GenericMonoSingleton<GameService>
+    {
+        [SerializeField] public PlayerScriptableObject playerScriptableObject;
+
+        public PlayerService playerService { get; private set; }
+
+        void Start()
+        {
+            playerService = new PlayerService(playerScriptableObject);
+        }
+
+        public void Update()
+        {
+            if (playerService != null)
+            {
+                playerService.CheckForInput();
+            }
+        }
+    }
+}
